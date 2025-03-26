5bb29b8670dbafc417a0c0d3c08aab090b159fd9
etcetera <yilonglei790@gmail.com>
2025-03-26 23:06:37 +0800

    feat(feature-ui): ✨ Add feature-ui package with initial components and configurations
    - Introduced the feature-ui package, including essential files such as package.json, tsconfig.json, and postcss.mjs.
    - Added components like Toolbar, ShadCNComponentsContext, and EtcDocView to enhance the UI functionality.
    - Configured Tailwind CSS and integrated Radix UI components for improved styling and interactivity.
    - Updated pnpm-lock.yaml to reflect new dependencies and ensure compatibility across the project.

---
+++ packages/feature-ui/src/ShadCNComponentsContext.tsx
@@ -0,0 +1,47 @@
+import { createContext, useContext } from 'react'
+
+import { Button as ShadCNButton } from '@doc-editor/ui/button'
+import {
+	Select as ShadCNSelect,
+	SelectContent as ShadCNSelectContent,
+	SelectItem as ShadCNSelectItem,
+	SelectTrigger as ShadCNSelectTrigger,
+	SelectValue as ShadCNSelectValue,
+} from '@doc-editor/ui/select'
+import { Toggle as ShadCNToggle } from '@doc-editor/ui/toggle'
+import {
+	Tooltip as ShadCNTooltip,
+	TooltipContent as ShadCNTooltipContent,
+	TooltipProvider as ShadCNTooltipProvider,
+	TooltipTrigger as ShadCNTooltipTrigger,
+} from '@doc-editor/ui/tooltip'
+
+export const ShadCNDefaultComponents = {
+	Button: { Button: ShadCNButton },
+	Select: {
+		Select: ShadCNSelect,
+		SelectContent: ShadCNSelectContent,
+		SelectItem: ShadCNSelectItem,
+		SelectTrigger: ShadCNSelectTrigger,
+		SelectValue: ShadCNSelectValue,
+	},
+	Toggle: {
+		Toggle: ShadCNToggle,
+	},
+	Tooltip: {
+		Tooltip: ShadCNTooltip,
+		TooltipContent: ShadCNTooltipContent,
+		TooltipProvider: ShadCNTooltipProvider,
+		TooltipTrigger: ShadCNTooltipTrigger,
+	},
+}
+
+export type ShadCNComponents = typeof ShadCNDefaultComponents
+
+export const ShadCNComponentsContext = createContext<ShadCNComponents | undefined>(
+	undefined,
+)
+
+export function useShadCNComponentsContext() {
+	return useContext(ShadCNComponentsContext)
+}
